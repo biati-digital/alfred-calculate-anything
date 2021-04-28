62cb14c8dbcae4c2ca2b5dc55595c311c40a5409
biati-digital <manuel@biati.digital>
2021-04-28 00:19:39 -0500

    Updated clear

--- workflow/lib/clear.php
+++ workflow/lib/clear.php
@@ -5,42 +5,29 @@
  * clears the workflow cache
  * and configuration
  */
+ 
+require_once getcwd() . '/alfred/Alfred.php';
 
-require_once 'functions.php';
+$translations  = \Alfred\getTranslation('config');
+$cache_dir = \Alfred\getDataPath('cache');
 
-$type = (isset($argv[1]) && !empty($argv[1]) ? $argv[1] : false);
-$cache_dir = getDataPath('cache');
-$message = '';
-
-if (!$type || !$cache_dir) {
-    return false;
+if (!$cache_dir || !file_exists($cache_dir)) {
+    echo $translations['cache_deleted'];
+    return true;
 }
 
-if ($type == 'cache' || $type == 'all') {
-    if (!file_exists($cache_dir)) {
-        return true;
-    }
+// Delete all children.
+$files = new \RecursiveIteratorIterator(
+    new \RecursiveDirectoryIterator($cache_dir, \RecursiveDirectoryIterator::SKIP_DOTS),
+    \RecursiveIteratorIterator::CHILD_FIRST
+);
 
-    // Delete all children.
-    $files = new \RecursiveIteratorIterator(
-        new \RecursiveDirectoryIterator($cache_dir, \RecursiveDirectoryIterator::SKIP_DOTS),
-        \RecursiveIteratorIterator::CHILD_FIRST
-    );
-
-    foreach ($files as $fileinfo) {
-        $action = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
-        if (!@$action($fileinfo->getRealPath())) {
-            return false;
-        }
+foreach ($files as $fileinfo) {
+    $action = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+    if (!@$action($fileinfo->getRealPath())) {
+        return false;
     }
 }
 
-if ($type == 'settings' || $type == 'all') {
-    $settings = getSettingsPath();
-
-    if (!empty($settings) && (is_file($settings) || is_link($settings))) {
-        unlink($settings);
-    }
-}
-
+echo $translations['cache_deleted'];
 return true;
