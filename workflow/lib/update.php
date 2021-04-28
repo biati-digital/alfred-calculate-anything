a020d98e32c7bcc531c6f77635107e2016c4d3b6
biati-digital <manuel@biati.digital>
2021-04-28 00:20:33 -0500

    Updated

---
+++ workflow/lib/update.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Check for updates
+ * check for available updates
+ */
+
+require_once getcwd() . '/alfred/Alfred.php';
+require_once getcwd() . '/workflow/calculateanything.php';
+
+$translations  = \Alfred\getTranslation('general');
+$calculate = new \Workflow\CalculateAnything();
+$updater = $calculate->getUpdater();
+$update_check = $updater->checkForUpdates(true);
+
+if (!$update_check || !isset($update_check['update_available']) || !$update_check['update_available']) {
+    echo $translations['no_updates'];
+    die();
+}
+
+$updater->notify($translations['update_downloading']);
+$updater->downloadUpdate();
+
+echo $translations['update_downloaded'];
