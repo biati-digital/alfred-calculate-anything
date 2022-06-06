6da31ef8d3128e601d4da5c4030aff351dd9e99d
biati-digital <manuel@biati.digital>
2022-06-05 23:10:33 -0500

    Added clear method

--- workflow/lib/clear.php
+++ workflow/lib/clear.php
@@ -9,25 +9,8 @@
 require_once getcwd() . '/alfred/Alfred.php';
 
 $translations  = \Alfred\getTranslation('config');
-$cache_dir = \Alfred\getDataPath('cache');
 
-if (!$cache_dir || !file_exists($cache_dir)) {
-    echo $translations['cache_deleted'];
-    return true;
-}
-
-// Delete all children.
-$files = new \RecursiveIteratorIterator(
-    new \RecursiveDirectoryIterator($cache_dir, \RecursiveDirectoryIterator::SKIP_DOTS),
-    \RecursiveIteratorIterator::CHILD_FIRST
-);
-
-foreach ($files as $fileinfo) {
-    $action = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
-    if (!@$action($fileinfo->getRealPath())) {
-        return false;
-    }
-}
+$clear = \Alfred\emptyCacheDirectory();
 
 echo $translations['cache_deleted'];
 return true;
