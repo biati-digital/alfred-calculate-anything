dabaf8d576ff7a42ee021146f6f6685b7c2e3daa
biati-digital <manuel@biati.digital>
2023-03-14 21:31:35 -0600

    Improved units conversion

--- workflow/tools/units.php
+++ workflow/tools/units.php
@@ -26,151 +26,219 @@ class Units extends CalculateAnything implements CalculatorInterface
     private $stop_words;
     private $keywords;
     private $unitsList;
+    private $unitsInfo;
     private $fractionUnits;
     private $lang;
     private $match_units;
+
 
     /**
      * Construct
      */
     public function __construct($query)
     {
-        $this->query = str_replace(',', '', $query);
+        $this->query = $query;
         $this->lang = $this->getTranslation('units');
         $this->keywords = $this->getKeywords('units');
         $this->stop_words = $this->getStopWords('units');
-        $this->unitsList = $this->availableUnits();
         $this->fractionUnits = $this->fractionUnits();
+
+        $this->buildUnitsData();
     }
+
+
+    /**
+     * Define all available units
+     */
+    private function units()
+    {
+        return [
+            'length' => [
+                'base' => 'm',
+                'units' => [
+                    'm' => 1, //meter
+                    'km' => 1000, //kilometer
+                    'dm' => 0.1, //decimeter
+                    'cm' => 0.01, //centimeter
+                    'mm' => 0.001, //milimeter
+                    'μm' => 0.000001, //micrometer
+                    'nm' => 0.000000001, //nanometer
+                    'pm' => 0.000000000001, //picometer
+                    'in' => 0.0254, //inch
+                    'ft' => 0.3048, //foot
+                    'yd' => 0.9144, //yard
+                    'mi' => 1609.344, //mile
+                    'nmi' => 1852, //nautical miles
+                    'h' => 0.1016, //hand
+                    'ly' => 9460730472580800, //lightyear
+                    'au' => 149597870700, //astronomical unit
+                    'pc' => 30856775814913672.789139379577965, //parsec
+                ]
+            ],
+            'area' => [
+                'base' => 'm2',
+                'units' => [
+                    'm2' => 1, //meter square - base unit for area
+                    'km2' => 1000000, //kilometer square
+                    'cm2' => 0.0001, //centimeter square
+                    'mm2' => 0.000001, //milimeter square
+                    'ft2' => 0.092903, //foot square
+                    'mi2' => 2589988.11, //mile square
+                    'ac' => 4046.86, //acre
+                    'ha' => 10000, //hectare
+                ]
+            ],
+            'volume' => [
+                'base' => 'l',
+                'units' => [
+                    'l' => 1, //litre - base unit for volume
+                    'dm3' => 1, //cubic decimeter - litre
+                    'ml' => 0.001, //mililitre
+                    'cm3' => 0.001, //cubic centimeter - mililitre
+                    'hl' => 100, //hectolitre
+                    'kl' => 1000, //kilolitre
+                    'm3' => 1000, //meters cubed - kilolitre
+                    'pt' => 0.473176, //pint
+                    'uspt' => 0.473176, //us pint
+                    'ukpt' => 0.56826125, //pint
+                    'gal' => 3.78541, //gallon
+                    'usgal' => 3.78541, //us gallon
+                    'ukgal' => 4.405, //uk gallon
+                    'qt' => 0.946353, //quart
+                    'usqt' => 0.946353, //us quart
+                    'ukqt' => 1.1365225, //uk imperial quart
+                    'yd3' => 764.55485798, //cubic yard
+                    'ft3' => 28.316846592, //cubic feet
+                    'in3' => 0.016387064, //cubic inches
+                    'floz' => 0.0295735, //Fluid ounces
+                ]
+            ],
+            'weight' => [
+                'base' => 'kg',
+                'units' => [
+                    'kg' => 1, //kilogram - base unit for weight
+                    'g' => 0.001, //gram
+                    'mg' => 0.000001, //milligram
+                    'N' => 9.80665002863885, //Newton (based on earth gravity)
+                    'st' => 6.35029, //stone
+                    'lb' => 0.453592, //pound
+                    'oz' => 0.028349523125, //ounce
+                    't' => 1000, //metric tonne
+                    'ukt' => 1016.047, //UK Long Ton
+                    'ust' => 907.1847, //US short Ton
+                ]
+            ],
+            'speed' => [
+                'base' => 'mps',
+                'units' => [
+                    'mps' => 1, //meter per second - base unit for speed
+                    'kph' => 0.277778, //kilometer per hour
+                    'mph' => 0.44704, //miles per hour
+                    'fps' => 0.3048, //feet per second
+                    'knot' => 0.514444, //knot
+                ]
+            ],
+            'rotation' => [
+                'base' => 'deg',
+                'units' => [
+                    'deg' => 1, //degrees - base unit for rotation
+                    'rad' => 57.2958, //radian
+                ]
+            ],
+            'temperature' => [
+                'base' => 'k',
+                'units' => [
+                    'k' => 1, //kelvin - base unit for temperature
+                    'c' => function ($val, $to_from) {
+                        return $to_from ? $val - 273.15 : $val + 273.15;
+                    },
+                    'f' => function ($val, $to_from) {
+                        return $to_from ? ($val * 9 / 5 - 459.67) : (($val + 459.67) * 5 / 9);
+                    },
+                ]
+            ],
+            'pressure' => [
+                'base' => 'pa',
+                'units' => [
+                    'pa' => 1, //Pascal - base unit for Pressure
+                    'hpa' => 100, //hpa
+                    'kpa' => 1000, //kilopascal
+                    'mpa' => 1000000, //megapascal
+                    'bar' => 100000, //bar
+                    'mbar' => 100, //milibar
+                    'psi' => 6894.76, //pound-force per square inch
+                ]
+            ],
+            'time' => [
+                'base' => 's',
+                'units' => [
+                    's' => 1, //second - base unit for time
+                    'year' => 31536000, //year - standard year
+                    'month' => 2628000, //month - 31 days
+                    'week' => 604800, //week
+                    'day' => 86400, //day
+                    'hr' => 3600, //hour
+                    'min' => 60, //minute
+                    'ms' => 0.001, //millisecond
+                    'μs' => 0.000001, //microsecond
+                    'ns' => 0.000000001, //nanosecond
+                ]
+            ],
+            'energy' => [
+                'base' => 'j',
+                'units' => [
+                    'j' => 1, //joule - base unit for energy
+                    'kj' => 1000, //kilojoule
+                    'mj' => 1000000, //megajoule
+                    'cal' => 4184, //calorie
+                    'Nm' => 1, //newton meter
+                    'ftlb' => 1.35582, //foot pound
+                    'whr' => 3600, //watt hour
+                    'kwhr' => 3600000, //kilowatt hour
+                    'mwhr' => 3600000000, //megawatt hour
+                    'mev' => 0.00000000000000016, //mega electron volt
+                ]
+
+            ],
+            'power' => [
+                'base' => 'w',
+                'units' => [
+                    'w' => 1, //watt - base unit for power
+                    'kw' => 1000, // kilowatt
+                    'ps' => 735.5, //metric horsepower
+                    'hp' => 745.7, // mechanical horsepower
+                ]
+            ]
+        ];
+    }
+
 
     /**
      * List of available units
      *
      * @return array
      */
-    private function availableUnits()
+    private function buildUnitsData()
     {
-        return [
-            'length' => [
-                'm',
-                'km',
-                'dm',
-                'cm',
-                'mm',
-                'μm',
-                'nm',
-                'pm',
-                'in',
-                'ft',
-                'yd',
-                'mi',
-                'nmi',
-                'h',
-                'ly',
-                'au',
-                'pc',
-            ],
-            'area' => [
-                'm2',
-                'km2',
-                'cm2',
-                'mm2',
-                'ft2',
-                'mi2',
-                'ac',
-                'ha',
-                'ha',
-            ],
-            'volume' => [
-                'dm3',
-                'l',
-                'ml',
-                'cm3',
-                'hl',
-                'kl',
-                'm3',
-                'pt',
-                'uspt',
-                'ukpt',
-                'gal',
-                'qt',
-                'usqt',
-                'ukqt',
-                'yd3',
-                'ft3',
-                'in3',
-                'floz',
-                'usgal',
-                'ukgal',
-            ],
-            'weight' => [
-                'kg',
-                'g',
-                'mg',
-                'N',
-                'st',
-                'lb',
-                'oz',
-                't',
-                'ukt',
-                'ust',
-            ],
-            'speed' => [
-                'mps',
-                'kph',
-                'mph',
-                'fps',
-                'knot',
-            ],
-            'rotation' => [
-                'deg',
-                'rad',
-            ],
-            'temperature' => [
-                'k',
-                'c',
-                'f',
-            ],
-            'pressure' => [
-                'pa',
-                'kpa',
-                'mpa',
-                'bar',
-                'mbar',
-                'psi',
-            ],
-            'time' => [
-                's',
-                'year',
-                'month',
-                'week',
-                'day',
-                'hr',
-                'min',
-                'ms',
-                'μs',
-                'ns',
-            ],
-            'energy' => [
-                'j',
-                'kj',
-                'mj',
-                'cal',
-                'Nm',
-                'ftlb',
-                'whr',
-                'kwhr',
-                'mwhr',
-                'mev',
-            ],
-            'power' => [
-                'w',
-                'kw',
-                'ps',
-                'hp',
-            ]
-        ];
+
+        $registered_units = $this->units();
+        $units = [];
+        $units_cats = [];
+
+        foreach ($registered_units as $category => $data) {
+            $units[$category] = array_keys($data['units']);
+
+            foreach ($units[$category] as $key => $value) {
+                $units_cats[$value] = $category;
+            }
+        }
+
+        $this->unitsList = $units;
+        $this->unitsInfo = $units_cats;
+
+        return $units;
     }
+
 
     private function fractionUnits()
     {
@@ -219,7 +287,7 @@ class Units extends CalculateAnything implements CalculatorInterface
         $stopwords = $this->getStopWordsString($this->stop_words);
         $this->match_units = $units;
 
-        return preg_match('/^\d*\.?\d+ ?' . $units . ' ?' . $stopwords . '? ' . $units . '$/i', $query, $matches);
+        return preg_match('/^([-\d+\.,\s]*) ?' . $units . ' ?' . $stopwords . '? ' . $units . '$/i', $query, $matches);
     }
 
 
@@ -313,52 +381,38 @@ class Units extends CalculateAnything implements CalculatorInterface
      */
     public function convert($data)
     {
-        $from = $data['from'];
-        $to = $data['to'];
         $amount = $data['amount'];
-        $from_type = $this->getUnitType($from);
-        $to_type = $this->getUnitType($to);
+        $can_convert = $this->getConversionCategory($data['from'], $data['to']);
 
-        if (empty($from_type) || empty($to_type)) {
+        if (empty($can_convert)) {
             return false;
         }
 
-        if ($to_type !== $from_type) {
-            return sprintf($this->lang['error'], $this->standardUnit($from), $this->standardUnit($to));
+        $category = isset($can_convert['category']) ? $can_convert['category'] : '';
+        $from = isset($can_convert['from']) ? $can_convert['from'] : '';
+        $to = isset($can_convert['to']) ? $can_convert['to'] : '';
+
+        if (!$can_convert) {
+            return sprintf($this->lang['error'], $this->standardUnit($data['from']), $this->standardUnit($data['to']));
         }
 
-        if ($from == 'year' && $to == 'month') {
-            $converted = $amount * 12;
-        } elseif ($from == 'month' && $to == 'year') {
-            $converted = $amount / 12;
-        } else {
-            $conversion_error = false;
-            try {
-                require_once dirname(__DIR__, 1) . '/lib/units/Convertor.php';
-                $convert = new \Convertor($amount, $from);
-                $converted = $convert->to($to, 2, true);
-            } catch (\Throwable $th) {
-                $conversion_error = $th->getMessage();
-            }
+        $converted = $this->convertTo($amount, $from, $to);
 
-            if ($conversion_error) {
-                return $conversion_error;
+        if (in_array($to, $this->fractionUnits) && fmod($converted, 1) > 0) {
+            if (isset($this->fractionUnits[$to])) {
+                $fraction = $this->convertTo(fmod($converted, 1), $to, $this->fractionUnits[$to]);
+                $fraction_unit = $this->fractionUnits[$to];
             }
-        }
-        if (in_array($to, $this->fractionUnits) and fmod($converted, 1) > 0) {
-            $convert->from(fmod($converted, 1), $to);
-            $fraction = $convert->to($this->fractionUnits[$to]);
-            $fraction_unit = $this->fractionUnits[$to];
         }
 
         $decimals = -1;
-        if ($from_type == 'temperature') {
+        if ($category == 'temperature') {
             $decimals = 1;
         }
 
         // Before displaying the result
         // Convert some units to readable human form
-        if ($from_type == 'time') {
+        if ($category == 'time') {
             if ($converted > 1) {
                 $human_readable = [
                     'ms' => 'milliseconds',
@@ -410,7 +464,7 @@ class Units extends CalculateAnything implements CalculatorInterface
         $query = str_replace(',', '', $query);
         $stopwords = $this->getStopWordsString($this->stop_words);
 
-        preg_match('/^(\d*\.?\d+) ?' . $this->match_units . ' ?' . $stopwords . '? ' . $this->match_units . '$/i', $query, $matches);
+        preg_match('/^([-\d+\.,\s]*) ?' . $this->match_units . ' ?' . $stopwords . '? ' . $this->match_units . '$/i', $query, $matches);
 
         if (empty($matches)) {
             return false;
@@ -426,7 +480,7 @@ class Units extends CalculateAnything implements CalculatorInterface
         }
 
         return [
-            'amount' => $this->cleanupNumber($amount),
+            'amount' => $this->cleanupNumber(str_replace(' ', '', $amount)),
             'from' => $from,
             'to' => $to,
         ];
@@ -444,16 +498,78 @@ class Units extends CalculateAnything implements CalculatorInterface
     private function getUnitType($unit)
     {
         $unit = str_replace('**', '', $unit);
-        $units = $this->unitsList;
-        $found = false;
-        foreach ($units as $key => $value) {
-            if (in_array($unit, $value)) {
-                $found = $key;
+
+        if (isset($this->unitsInfo[$unit])) {
+            return $this->unitsInfo[$unit];
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if a category
+     * has a specific unit
+     *
+     * @param $unit     unit to search
+     * @param $category category to check if has the unit
+     * @return boolean
+     */
+    private function getConversionCategory($from_unit, $to_unit)
+    {
+        $all_units = $this->units();
+        $found_from = [];
+        foreach ($all_units as $category => $value) {
+            $units = $value['units'];
+
+            foreach ($units as $key => $data) {
+                if ($from_unit === $key || $from_unit === strtolower($key)) {
+                    $found_from[$key] = $category;
+                }
+            }
+        }
+
+        if (empty($found_from)) {
+            return false;
+        }
+
+        $result = [];
+        foreach ($found_from as $unit => $unit_category) {
+            $set = $all_units[$unit_category];
+            foreach ($set['units'] as $key => $data) {
+                if ($to_unit === $key || $to_unit === strtolower($key)) {
+                    $result['from'] = $unit;
+                    $result['to'] = $key;
+                    $result['category'] = $unit_category;
+                    break;
+                }
+            }
+
+            if (!empty($result)) {
                 break;
             }
         }
 
-        return $found;
+        return $result;
+    }
+
+
+    /**
+     * Check if a category
+     * has a specific unit
+     *
+     * @param $unit     unit to search
+     * @param $category category to check if has the unit
+     * @return boolean
+     */
+    private function categoryHasUnit($unit, $category)
+    {
+        $units = $this->units();
+        $set = $units[$category];
+        $cat_has_unit = array_filter(array_keys($set['units']), function ($item) use ($unit) {
+            return strtolower($item) === $unit;
+        });
+
+        return $cat_has_unit;
     }
 
 
@@ -465,15 +581,7 @@ class Units extends CalculateAnything implements CalculatorInterface
      */
     private function isValidUnit($unit)
     {
-        $units = $this->unitsList;
-        $found = false;
-        foreach ($units as $value) {
-            if (in_array($unit, $value)) {
-                $found = true;
-                break;
-            }
-        }
-        return $found;
+        return isset($this->unitsInfo[$unit]);
     }
 
 
@@ -609,5 +717,32 @@ class Units extends CalculateAnything implements CalculatorInterface
         }
 
         return $list;
+    }
+
+
+    /**
+     * Make the actual conversion
+     */
+    private function convertTo($amount, $from_unit, $to_unit)
+    {
+        $from_type = $this->getUnitType($from_unit);
+        $base = 0;
+        $units = $this->units();
+        $set = $units[$from_type];
+
+        if (is_callable($set['units'][$from_unit])) {
+            $base = $set['units'][$from_unit]($amount, false);
+        } else {
+            $base = $amount * $set['units'][$from_unit];
+        }
+
+        //calculate converted value
+        if (is_callable($set['units'][$to_unit])) {
+            $result = $set['units'][$to_unit]($base, true);
+        } else {
+            $result = $base / $set['units'][$to_unit];
+        }
+
+        return $result;
     }
 }
