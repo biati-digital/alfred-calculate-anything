dec299161550bdc25b5b15425fdd232feb7c7e37
biati-digital <manuel@biati.digital>
2023-03-18 16:09:54 -0600

    Clear also env variable

--- workflow/lib/clear.php
+++ workflow/lib/clear.php
@@ -5,12 +5,13 @@
  * clears the workflow cache
  * and configuration
  */
- 
+
 require_once getcwd() . '/alfred/Alfred.php';
 
 $translations  = \Alfred\getTranslation('config');
 
 $clear = \Alfred\emptyCacheDirectory();
+\Alfred\setVariable('fixer_source_api', '', false);
 
 echo $translations['cache_deleted'];
 return true;
